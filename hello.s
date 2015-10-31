87d80f2a675107cc01672d94bab4ef4e5c5570d8
Adrien Thierry <adrienthierry2@gmail.com>
2015-10-31 12:54:40 -0400

    Launch script fixed (now includes creation of floppy image) + print a message on screen

--- hello.s
+++ hello.s
@@ -2,6 +2,27 @@
 		.data
 		.text
 		.global _start
+
 _start :
-		.org 510,0 
-		.word 0xaa55
+
+		mov %cs, %ax		# ax = 0x07c0
+		mov %ax, %ds		# ds = 0x07c0
+		mov %ax, %es		# es = 0x07c0
+		mov %ax, %ss		# ss = 0x07c0
+		mov $0x100, %sp		# 256 words in stack (512 bytes)
+
+		mov $0x3f,%bl	# Background and text color
+		mov $0,%dh		# Column 0
+		mov $0,%dl		# Line 0
+		mov $0x1301,%ax	# Function to print a string
+		mov $msg,%bp	# Message address
+		mov $13,%cx		# Message size 
+		int $0x10		# Interrupt
+
+msg:
+		.asciz "Hello World !"
+		.org 510
+
+signature:
+
+		.word 0xaa55	# Make floppy bootable
--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 make clean
 make
-qemu-system-i386 -drive file=floppy.vfd,if=floppy,format=raw -m 32
+dd if=hello of=floppy.vfd
+qemu-system-i386 -monitor stdio -drive file=floppy.vfd,if=floppy,format=raw -m 32
 
 exit 0
