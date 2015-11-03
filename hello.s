02670f452a3838ee9853539fa6657597034e0327
Adrien Thierry <adrienthierry2@gmail.com>
2015-11-03 13:22:26 -0500

    Print is now in a function and parameters are passed on the stack

--- hello.s
+++ hello.s
@@ -1,4 +1,5 @@
 		.code16
+
 		.data
 		.text
 		.global _start
@@ -11,18 +12,43 @@ _start :
 		mov %ax, %ss		# ss = 0x07c0
 		mov $0x100, %sp		# 256 words in stack (512 bytes)
 
-		mov $0x3f,%bl	# Background and text color
-		mov $0,%dh		# Column 0
-		mov $0,%dl		# Line 0
-		mov $0x1301,%ax	# Function to print a string
-		mov $msg,%bp	# Message address
-		mov $13,%cx		# Message size 
-		int $0x10		# Interrupt
+		mov $0x01, %bl		# Background and text color
+		mov $0x00, %dl		# Line number
+
+		push $msg			# Message address
+		push $13			# Message size
+
+		mov loop_count, %cx
+		jmp print
+
+print :
+		mov %sp, %bp		# Copy sp in bp
+
+		push %cx			# Save loop counter
+
+		mov $0,%dl			# Column 0
+		mov $0x1301,%ax		# Function to print a string
+		mov 0(%bp), %cx		# Message size
+		mov 2(%bp), %bp		# Message address
+		int $0x10			# Interrupt
+
+		incb %bl			# Change color
+		incb %dh			# Change line
+
+		pop %cx				# Restore loop counter
+
+		loop print			# Loop
+		
+idle :
+		hlt
+		jmp idle
+
+loop_count:
+		.word 10			# Number of iterations
 
 msg:
 		.asciz "Hello World !"
-		.org 510
 
 signature:
-
-		.word 0xaa55	# Make floppy bootable
+		.org 510
+		.word 0xaa55		# Make floppy bootable
